1b7687a713c73b14764ea2d23a21b50a0bd47df4
tai0116 <taisukekacho@gmail.com>
2021-04-12 19:57:25 +0900

    途中

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(MyApp(
-      MaterialApp(
-          home: Scaffold(
-            backgroundColor: Colors.greenAccent,
-            appBar: AppBar(
-            title: Text('HOME'),
-          ;
+  runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   @override
@@ -20,13 +13,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyDemo(),
-
-
     );
   }
 }
-
-
 
 class MyDemo extends StatelessWidget {
   final List<Tab> myTabs = <Tab>[
@@ -39,102 +28,14 @@ class MyDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: myTabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: TabBar(
-            tabs: myTabs,
+        length: myTabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: myTabs,
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: myTabs.map((Tab tab) {
-            final String label = tab.text!.toLowerCase();
-            return Center(
-              child: Text(
-                'This is the $label tab',
-                style: const TextStyle(fontSize: 36),
-              ),
-            );
-          }).toList(),
-        ),
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Settings',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  class MyDemo extends StatelessWidget {
-    @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-            backgroundColor: Colors.purple,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-            backgroundColor: Colors.pink,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
+          body: TabBarView(children: []),
+        ));
   }
 }
