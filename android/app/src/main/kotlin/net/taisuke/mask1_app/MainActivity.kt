aed14dfdeac27750439eebbbd534450aae0f54ae
tai0116 <taisukekacho@gmail.com>
2021-04-12 15:29:54 +0900

    no message

---
+++ android/app/src/main/kotlin/net/taisuke/mask1_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.taisuke.mask1_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyDemo(),
+    );
+  }
+}
+
+class MyDemo extends StatelessWidget {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: 'こども'),
+    Tab(text: '小さい'),
+    Tab(text: '普通'),
+    Tab(text: '大きい'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: myTabs,
+          ),
+        ),
+        body: TabBarView(
+          children: myTabs.map((Tab tab) {
+            final String label = tab.text!.toLowerCase();
+            return Center(
+              child: Text(
+                'This is the $label tab',
+                style: const TextStyle(fontSize: 36),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
