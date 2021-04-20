0098d760f2489252a392f5a561f579b6f2d28600
tai0116 <taisukekacho@gmail.com>
2021-04-20 21:27:07 +0900

    no message

---
+++ lib/screens/bottom_navigation_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavBottom extends StatefulWidget {
+  @override
+  _NavBottomState createState() => _NavBottomState();
+}
+
+class _NavBottomState extends State<NavBottom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'bottom_navigation_screen.dart';
+
 class SecondPage extends StatefulWidget {
   SecondPage(this.title);
   final String title;
@@ -9,14 +11,43 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  int selectedIndex = 0;
+  List widgetOptions = [
+    NavBottom(),
+    NavBottom(),
+    NavBottom(),
+    NavBottom(),
+    NavBottom(),
+  ];
+
+  void onItemTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        height: double.infinity,
+      body: Center(
+        child: widgetOptions.elementAt(selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_back),
+            title: Text('少し小さい'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_forward),
+            title: Text('少し大きい'),
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: onItemTap,
       ),
     );
   }
